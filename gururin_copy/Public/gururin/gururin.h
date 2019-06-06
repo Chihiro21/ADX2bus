7a6507dcbbf1fef4f8d56ce3f60c988cff05dd3d
山田千尋 <>
2019-06-06 17:19:58 +0900

    ADX2のフォルダをcopy

---
+++ gururin_copy/Public/gururin/gururin.h
@@ -0,0 +1,29 @@
+﻿/*===========================================================================*
+ *  Header file for Atom CueSheet Binary
+ *  Project          : gururin
+ *  Tool Ver.        : Ver.3.41.10
+ *  ACB  Ver.        : Ver.1.32.00
+ *  File Path        : /Users/yamadachihiro/Documents/CRIWARE/CriAtomCraft/gururinのコピー/Public/gururin
+ *  File Name        : gururin.h
+ *  File Size        : 1429536 bytes
+ *  Date Time        : 2019/05/30 20:01:00
+ *  Target           : Public
+ *  Cues             : 4
+ *  CueSheet Comment : 
+ *  Stream Awb Path  : /Users/yamadachihiro/Documents/CRIWARE/CriAtomCraft/gururinのコピー/Public/gururin/gururin.awb
+ *===========================================================================*/
+
+#define CRI_GURURIN_CUENUM (4)
+
+/* AISAC Control List (AISAC Control ID) */
+
+/* Cue List (Cue ID) */
+#define CRI_GURURIN_SE_JUMP              ( 1) /*  */
+#define CRI_GURURIN_SE_FOOT              ( 2) /*  */
+#define CRI_GURURIN_SE_RACK              ( 3) /*  */
+#define CRI_GURURIN_BGM_TUTOLIAL         ( 4) /*  */
+
+/* Block List (Block Index) */
+
+/* end of file */
+
